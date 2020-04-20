816f856f1d13886c7f51f16a1ecfc581a8f8fd3a
saket61195 <saket61195@gmail.com>
2020-04-20 16:09:00 +0530

    group chat completed

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -7,8 +7,8 @@ class MessagesController < ApplicationController
   def create
     message = @chatroom.messages.new(message_params)
     message.user = current_user
-
     message.save
+    MessageRelayJob.perform_later(message)
   end
 
   private
