95ee91cb49e3b2fa50634d03670a58c80c7b8c4c
saket61195 <saket61195@gmail.com>
2020-04-21 13:27:36 +0530

    chatroom model update

--- app/models/chatroom.rb
+++ app/models/chatroom.rb
@@ -1,7 +1,7 @@
 class Chatroom < ApplicationRecord
-	has_many :chatroom_users
+	has_many :chatroom_users, dependent: :destroy
 	has_many :users, through: :chatroom_users
-	has_many :messages
+	has_many :messages, dependent: :destroy
 
 	scope :public_channels, ->{where(direct_message: false)}
 	scope :direct_messages, ->{where(direct_message: true)}
